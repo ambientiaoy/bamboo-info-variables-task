441b080d72529cf6acc6c8634e6ce2118debc832
Gavin Bunney <gavin.bunney@gmail.com>
2012-08-29 09:20:58 +1000

    Updated copyright notice

--- src/main/java/au/net/bunney/bamboo/plugins/infovarstask/InfoVariablesTask.java
+++ src/main/java/au/net/bunney/bamboo/plugins/infovarstask/InfoVariablesTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Gavin Bunney, Brisbane, Australia.
+ * Copyright 2012 Bunney Apps, Brisbane, Australia.
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
